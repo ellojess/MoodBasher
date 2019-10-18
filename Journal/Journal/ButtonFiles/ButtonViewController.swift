ecf7a8030232eb9001f93dc2561479256b3788dc
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-10-18 00:22:07 -0700

    add button and count functionality

--- Journal/Journal/ButtonFiles/ButtonViewController.swift
+++ Journal/Journal/ButtonFiles/ButtonViewController.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2019 Jessica Trinh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RageButton: UIViewController {
+    
+}
--- Journal/Journal/JournalFiles/ViewController.swift
+++ Journal/Journal/JournalFiles/ViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var rageButton: UIButton!
+    
+    var count: Int = 0
+    var counting: Bool = false
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func rageButton(_ sender: UIButton) {
+        if counting {
+            //stop counting
+        } else if !counting {
+            //start counting
+            rageButton.setTitle("Stop Counting", for: .normal)
+            //start timer
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(counter), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func counter() -> Void {
+        
+        count += 1
+        countLabel.text = "Count: " + String(count)
     }
 
 
